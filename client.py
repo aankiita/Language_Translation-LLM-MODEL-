f9c029a742297b646da3d3ee2d50242c47c09f7c
ANKITA YADAV <145215095+aankiita@users.noreply.github.com>
2026-02-24 19:49:34 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,43 @@
+import requests
+import streamlit as st
+
+def get_groq_response(input_text, language):
+    json_body = {
+        "input": {
+            "language": language,   
+            "text": input_text
+        },
+        "config": {},
+        "kwargs": {}
+    }
+
+    response = requests.post(
+        "http://127.0.0.1:8000/chain/invoke",
+        json=json_body
+    )
+
+    result = response.json()
+    return result.get("output", "No output found")
+
+## Streamlit app
+st.title("LLM Language Translator Using LCEL")
+
+# User input
+input_text = st.text_input("Enter the text you want to translate")
+
+# Dropdown for language selection
+language = st.selectbox(
+   "Select target language",
+    [
+        "French", "Spanish", "German", "Italian", "Portuguese", 
+        "Dutch", "Russian", "Japanese", "Chinese", "Korean",
+        "Hindi", "Bengali", "Urdu", "Arabic", "Turkish",
+        "Swedish", "Norwegian", "Polish", "Greek", "Vietnamese",
+        "Thai", "Indonesian"
+    ]
+)
+
+# Translate on input
+if input_text:
+    translation = get_groq_response(input_text, language)
+    st.write("**Translated Text:**", translation)
